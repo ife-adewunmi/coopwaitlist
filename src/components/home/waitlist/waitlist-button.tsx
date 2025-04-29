1e398851bf3c3397ce1a9d1cbba9b7b293bbdc9b
Ife-adewunmi <ifeoluwa.adewunmi94@gmail.com>
2025-04-29 16:50:51 +0100

    [fix:] format style

--- src/components/home/waitlist/waitlist-button.tsx
+++ src/components/home/waitlist/waitlist-button.tsx
@@ -1,53 +1,49 @@
-import React from 'react';
-import { Button, ButtonProps } from '@/components/ui/button';
-import { ButtonLabel } from '@/components/ui/button-label';
-import Link from 'next/link';
+import React from 'react'
+import { Button, ButtonProps } from '@/components/ui/button'
+import { ButtonLabel } from '@/components/ui/button-label'
+import Link from 'next/link'
 
 interface WaitlistButtonProps extends ButtonProps {
-    label?: string
-    asLink?: boolean
-    className?: string
-    icon?: React.ReactNode
-    onClick?: () => void
+  label?: string
+  asLink?: boolean
+  className?: string
+  icon?: React.ReactNode
+  onClick?: () => void
 }
 
 export const WaitlistButton = ({
-    label = 'Join Waitlist',
-    asLink = false,
-    className,
-    icon,
-    ...props
-  }: WaitlistButtonProps) => {
-    const handleWaitlistClick = (event?: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
-        if (asLink && event) {
-            event.preventDefault();
-            const waitlistSection = document.getElementById('waitlist');
-            if (waitlistSection) {
-                waitlistSection.scrollIntoView({ behavior: 'smooth' });
-            }
-        }
-        if (props.onClick) {
-            props.onClick();
-        }
-    };
- 
-    if (asLink) {
-        return (
-            <Button asChild className={className} {...props}>
-                <Link href="#waitlist">
-                    <ButtonLabel label={label} buttonIcon={{icon: icon}} />
-                </Link>
-            </Button>
-        );
+  label = 'Join Waitlist',
+  asLink = false,
+  className,
+  icon,
+  ...props
+}: WaitlistButtonProps) => {
+  const handleWaitlistClick = (event?: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
+    if (asLink && event) {
+      event.preventDefault()
+      const waitlistSection = document.getElementById('waitlist')
+      if (waitlistSection) {
+        waitlistSection.scrollIntoView({ behavior: 'smooth' })
+      }
     }
+    if (props.onClick) {
+      props.onClick()
+    }
+  }
 
+  if (asLink) {
     return (
-        <Button
-            className={className}
-            onClick={handleWaitlistClick}
-            {...props}
-        >
-            <ButtonLabel label={label} buttonIcon={{icon: icon}} />
-        </Button>
-    );
+      <Button asChild className={className} {...props}>
+        <Link href="#waitlist">
+          <ButtonLabel label={label} buttonIcon={{ icon: icon }} />
+        </Link>
+      </Button>
+    )
+  }
+
+  return (
+    <Button className={className} onClick={handleWaitlistClick} {...props}>
+      <ButtonLabel label={label} buttonIcon={{ icon: icon }} />
+    </Button>
+  )
 }
--- src/components/ui/button-label.tsx
+++ src/components/ui/button-label.tsx
@@ -1,52 +1,49 @@
-import React from "react";
+import React from 'react'
 
-type IconPosition = "left" | "right";
+type IconPosition = 'left' | 'right'
 
 export interface ButtonIcon {
-    icon: React.ReactNode;
-    iconPosition?: IconPosition;
-    iconSize?: number;
-    iconColor?: string;
-    iconClassName?: string;
+  icon: React.ReactNode
+  iconPosition?: IconPosition
+  iconSize?: number
+  iconColor?: string
+  iconClassName?: string
 }
 
 export interface ButtonLabelProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    label?: string | React.ReactNode;
-    className?: string;
-    buttonIcon?: ButtonIcon;
+  label?: string | React.ReactNode
+  className?: string
+  buttonIcon?: ButtonIcon
 }
 
-export const ButtonLabel = ({
-    label,
-    buttonIcon
-}: ButtonLabelProps) => {
-    const {
-        icon,
-        iconPosition = "right",
-        iconSize = 16,
-        iconColor = "currentColor",
-        iconClassName = "h-4 w-4",
-    } = buttonIcon || {};
+export const ButtonLabel = ({ label, buttonIcon }: ButtonLabelProps) => {
+  const {
+    icon,
+    iconPosition = 'right',
+    iconSize = 16,
+    iconColor = 'currentColor',
+    iconClassName = 'h-4 w-4',
+  } = buttonIcon || {}
 
-    const iconStyles = {
-        width: iconSize,
-        height: iconSize,
-        color: iconColor,
-    };
+  const iconStyles = {
+    width: iconSize,
+    height: iconSize,
+    color: iconColor,
+  }
 
-    return (
-        <>
-            {icon && iconPosition === "left" && (
-                <span className={iconClassName} style={iconStyles}>
-                    {icon}
-                </span>
-            )}
-            { label }
-            {icon && iconPosition === "right" && (
-                <span className={iconClassName} style={iconStyles}>
-                    {icon}
-                </span>
-            )}
-        </>
-    );
-};
+  return (
+    <>
+      {icon && iconPosition === 'left' && (
+        <span className={iconClassName} style={iconStyles}>
+          {icon}
+        </span>
+      )}
+      {label}
+      {icon && iconPosition === 'right' && (
+        <span className={iconClassName} style={iconStyles}>
+          {icon}
+        </span>
+      )}
+    </>
+  )
+}
