23ce13b8b721576b15ceca246e9b3b50e4d04fb0
Tobicode <tobijoshua919@gmail.com>
2025-06-04 01:09:25 +0100

    Exported registration types from schema in database index

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,5 @@
 // Database connection and client exports
 export * from './connection'
 export * from './schema'
+
+export type { InsertRegistration, SelectRegistration } from './schema/registrations'
